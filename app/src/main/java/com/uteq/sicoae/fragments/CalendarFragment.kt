c8a18c62771d087dfc2174ca67fb137b87ec4c8a
JorgeCoronelG <JorgeCoronelG@users.noreply.github.com>
2019-12-14 00:01:48 -0600

    Diseño del módulo de calendario y estadísticas

--- app/src/main/java/com/uteq/sicoae/fragments/CalendarFragment.kt
+++ app/src/main/java/com/uteq/sicoae/fragments/CalendarFragment.kt
@@ -1,23 +1,35 @@
 package com.uteq.sicoae.fragments
 
-
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.Toast
 
 import com.uteq.sicoae.R
 
-class CalendarFragment : Fragment() {
+class CalendarFragment : Fragment(), CalendarView.OnDateChangeListener {
+
+    private var calendar: CalendarView? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_calendar, container, false)
+
+        calendar = view.findViewById(R.id.calendar)
+        calendar?.setOnDateChangeListener(this)
+
         return view;
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         activity?.title = resources.getString(R.string.calendar)
+    }
+
+    override fun onSelectedDayChange(view: CalendarView, year: Int, month: Int, dayOfMonth: Int) {
+        var date = "${dayOfMonth} / ${month} / ${year}"
+        Toast.makeText(context, date, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/uteq/sicoae/fragments/StadisticsFragment.kt
+++ app/src/main/java/com/uteq/sicoae/fragments/StadisticsFragment.kt
@@ -1,18 +1,32 @@
 package com.uteq.sicoae.fragments
 
-
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 import com.uteq.sicoae.R
 
 class StadisticsFragment : Fragment() {
 
+    var spnPeriod: Spinner? = null
+    var spnStudents: Spinner? = null
+    val periods = arrayOf("Selecciona:","Última semana","Último mes")
+    val students = arrayOf("Selecciona:")
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_stadistics, container, false)
+
+        spnPeriod = view.findViewById(R.id.spn_period)
+        spnStudents = view.findViewById(R.id.spn_student)
+        var arrayAdapter = ArrayAdapter(context, android.R.layout.simple_spinner_dropdown_item, periods)
+        spnPeriod?.adapter = arrayAdapter
+        arrayAdapter = ArrayAdapter(context, android.R.layout.simple_spinner_dropdown_item, students)
+        spnStudents?.adapter = arrayAdapter
+
         return view;
     }
 
